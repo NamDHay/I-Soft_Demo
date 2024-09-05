987378b0f0ea56c9ad5ebb7334a6e4c789c94cb8
NguyenNamHuy <nguyennamhuy2003@gmail.com>
2024-09-05 15:49:50 +0700

    bug: cannot send socket

--- application/route.py
+++ application/route.py
@@ -1,13 +1,32 @@
 from application import app, socketio
-from flask import render_template, request, url_for, redirect,flash,get_flashed_messages, jsonify, copy_current_request_context
 from flask_socketio import SocketIO, emit, disconnect
+from flask import render_template, request, url_for, redirect,flash,get_flashed_messages, jsonify, copy_current_request_context
 import json
 
 app.app_context().push()
 
+class FileData:
+    def read(self, file):
+        with open(file, 'r') as openfile:
+            json_object = json.load(openfile)
+            json_object = json.dumps(json_object)
+        return json_object
+    
+    def write(self, file, data):
+        with open(file, "w") as outfile:
+            json.dump(data, outfile, indent=4)  # Added indent for better readability
+        print("Data written to file successfully.")
+
+@socketio.on('connect')
+def handle_connect():
+    file_data = FileData()
+    data = file_data.read("application/Json/data.json")
+    print(data)
+    emit('Data', data)
+
 @app.route("/")
 def index():
-    return redirect(url_for('login'))
+    return redirect(url_for('dashboard'))
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -15,4 +34,8 @@ def login():
 
 @app.route("/dashboard", methods=["GET", "POST"])
 def dashboard():
+    file_data = FileData()
+    data = file_data.read("application/Json/data.json")
+    print(data)
+    socketio.emit('Data', data)
     return render_template("dashboard.html")  
--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from application import socketio
 app.app_context().push()
 if __name__ == "__main__":
     socketio.run(app,host="0.0.0.0", port=5000, use_reloader=True, debug=True)
-    app.run(host='0.0.0.0', port=5050, debug=True)
+    # app.run(host='0.0.0.0', port=5050, debug=True)
