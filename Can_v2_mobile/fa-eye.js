be9cf7af7e97e566d7b2f0afb3dcafe3b49f4d55
NguyenNamHuy <nguyennamhuy2003@gmail.com>
2024-09-12 15:23:26 +0700

    feat: Add Can mobile

---
+++ Can_v2_mobile/fa-eye.js
@@ -0,0 +1,14 @@
+document.getElementById('toggle-password').addEventListener('click', function() {
+    const passwordField = document.getElementById('password');
+    const icon = document.getElementById('toggle-icon');
+
+    if (passwordField.type === 'password') {
+        passwordField.type = 'text';
+        icon.classList.remove('fa-eye');
+        icon.classList.add('fa-eye-slash');
+    } else {
+        passwordField.type = 'password';
+        icon.classList.remove('fa-eye-slash');
+        icon.classList.add('fa-eye');
+    }
+});
